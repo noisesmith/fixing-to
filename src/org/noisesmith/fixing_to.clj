f75a0422b370931cfd4e6957567f1b66dee32ac9
Justin Smith <noisesmith@gmail.com>
2017-08-07 18:44:26 -0700

    trying this again

--- src/org/noisesmith/fixing_to.clj
+++ src/org/noisesmith/fixing_to.clj
@@ -44,6 +44,7 @@
         ~doc
         (~(symbol (str "map->" record-name)) {:doc ~doc})))))
 
+#_
 (comment (def-fixing-to foo
             "foo has keys a and b and prints the values of those keys,
              along with any additional args provided."
@@ -51,7 +52,13 @@
            ([this]
             (apply println a b args)))
          (assoc foo :a 0 :b 1)
-         #user.Fixing_fooRecord{:a 0, :b 1, :args nil, :doc "a foo"}
+         ;; #user.Fixing_fooRecord{:a 0, :b 1, :args nil, :doc "a foo"}
          ((assoc foo :a 0 :b 1) :a :b :c :d :e :f :g :h :i :j :k :l :m :n :o :p :q :r :s :t :u :v :w :x :y :z)
-         => 0 1 :a :b :c :d :e :f :g :h :i :j :k :l :m :n :o :p :q :r :s :t :u :v :w :x :y :z
+         ;; => 0 1 :a :b :c :d :e :f :g :h :i :j :k :l :m :n :o :p :q :r :s :t :u :v :w :x :y :z
          nil)
+
+(deftype AffixedFn
+  [m f]
+  Object
+  (toString [this]
+    (str f " affixing " m)))
